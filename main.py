db77f2bf0aaebfa44517e8cdfdb0a3446b4488e9
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-11-26 17:36:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from guietta import _, Gui, Quit
+#from PySide2 import QtWidgets (sim izskatas vajag py3.8.1)
 
+# pip install guietta
+# pip install PySide2
+# pip install PyQt5
+
+# https://guietta.readthedocs.io/en/stable/tutorial.html
 
 gui = Gui(
 
